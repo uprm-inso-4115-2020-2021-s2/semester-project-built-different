a407929bca98819ebfccdcb495eb79d33d67b21a
Luis <luis.caro1@upr.edu>
2021-03-06 13:53:26 -0400

    jest testing for passing user data to API

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -2,13 +2,13 @@
 //   LocalStrategy = require("passport-local");
 
 const signupPost = async (req, res) => {
-  console.log(req);
   // req contains paramaters that is passed to the route
   // req.body ---> pass information to the backend
   //const {email,password, name, lastname} = req.body
   // res is what you are going to return from the response
-
   res.json(req.body);
 };
+
+// const loginPost = async (req, res) => {};
 
 module.exports = { signupPost };
