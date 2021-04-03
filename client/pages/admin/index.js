79ee33116847ee53e1fca8aed311828f565929e9
Christian Rosado <c.emanuel72@gmail.com>
2021-04-03 11:08:28 -0400

    adding admin pages

---
+++ client/pages/admin/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Navbar from '../../components/Navbar';
+import styles from '../../styles/admin/Home.module.css';
+import { Button } from 'react-bootstrap';
+import { useRouter } from 'next/router';
+
+export default function Home() {
+    const router = useRouter();
+    return (
+        <>
+            <Navbar />
+            <div className={styles.container}>
+                <h2>Bienvenido, Usuario</h2>
+                <p>Acciones</p>
+                <hr/>
+                <div className={styles.buttonbox}>
+                    <Button variant="success" onClick={() => router.push('./admin/inventario')}>Ver inventario</Button>
+                    <Button variant="success" onClick={() => router.push('./admin/addfood')}>Añadir comida</Button>
+                    <Button variant="success">Editar comida</Button>
+                    <Button variant="success">Eliminar comida</Button>
+                </div>
+            </div>
+        </>
+    )
+}
